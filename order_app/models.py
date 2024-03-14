c9ed3f553fe60e0a8f3b1837e73b36c69284bf7b
klee0220 <kleeuz@gmail.com>
2024-03-14 13:54:17 +0500

    Created models

--- order_app/models.py
+++ order_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipment(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+
+class FieldAnalyzer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+
+
+class Request(models.Model):
+    request_type = models.CharField(max_length=100)
+    urgency = models.CharField(max_length=50)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50)
+    last_updated_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    analyzer = models.ForeignKey(FieldAnalyzer, on_delete=models.CASCADE)
